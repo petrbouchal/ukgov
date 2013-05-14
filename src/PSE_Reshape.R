28faf0df1a1414ebeab26ab0b4015f7411b78c33
Petr Bouchal <pbouchal@gmail.com>
2013-05-14 21:11:25 +0100

    Annotate PSE script

--- src/PSE_Reshape.R
+++ src/PSE_Reshape.R
@@ -3,6 +3,8 @@ library(stringr)
 library(ggthemes)
 library(reshape2)
 library(plyr)
+
+# load data
 change <- read.csv("./data-input/ChangeDeptsFTE.csv")
 names(change)
 
@@ -15,13 +17,11 @@ names(change) <- gsub("_to","", names(change), fixed=TRUE)
 names(change) <- gsub("_201",".201", names(change), fixed=TRUE)
 names(change)
 
+# remove and reorder variables
 change <- change[,c(1:42,51:70,45:47,50)]
 names(change)
 
-# remove variables here
-
-# need to adjust reshape arguments to work with this dataset
-
+# reshape
 changel <- melt(change)
 changel$variable <- gsub(".","-",changel$variable, fixed=TRUE) 
 changel$measure <- gsub("-.*","",changel$variable)
@@ -30,6 +30,7 @@ changel$Dept <- gsub(" ",".",changel$Dept)
 changel$variable <- NULL
 changel$group=paste(changel$Dept,changel$Whitehall,sep='-')
 
+# plot
 plotPSE <- ggplot(data=subset(changel,changel$measure=='Cumulative_perc_endog_change'),
                   aes(x=Period,y=value, group=group, colour=Whitehall)) + 
   geom_line(size=1) +
