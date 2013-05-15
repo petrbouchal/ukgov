3a0ce694cfd809b1cf219d8cb48e435da22a54ec
Petr Bouchal <pbouchal@gmail.com>
2013-05-15 15:25:02 +0100

    Edit to reshape to improve motion chart Edit to motion chart code

--- src/PSE_Charts.R
+++ src/PSE_Charts.R
@@ -1,5 +1,7 @@
 library(plyr)
-source('./src/PSE_Reshape.R') # to prepare data if needed
+library(reshape)
+
+# source('./src/PSE_Reshape.R') # to prepare data if needed
 
 # Manual for google charts:
 # 
@@ -18,7 +20,9 @@ plotPSE <- ggplot(data=subset(changel,changel$measure=='Cumulative_perc_endog_ch
 plotPSE
 
 # reshape to wide(r) for googlevis
-changel2 <- data.frame(cast(changel, ... ~ measure))
+#changel2 <- data.frame(cast(changel, ... ~ measure))
+changel$group <- NULL
+changel2 <- data.frame(cast(changel, ... ~ measure + Whitehall))
 
 # dates for googlevis
 changel2$Period <- gsub("Q1","-03-31",changel2$Period)
@@ -29,34 +33,40 @@ changel2$Period <- gsub("Q4","-12-31",changel2$Period)
 changel2$Period <- as.Date(changel2$Period, tz = "GMT", format='%Y-%m-%d')
 
 # Google Plot
+# TODO: rename variables
+# TODO: get percent scales
+# TODO: subset data for different charts (excl. totals, excl. abs OR relative vars)
+#       and develop separate charts
+# TODO: get the quarterly scale working
+# TODO: get default variable choices working
 suppressPackageStartupMessages(library(googleVis))
-Motion=gvisMotionChart(changel2, idvar="group", timevar="Period",
+Motion=gvisMotionChart(changel2, idvar="Dept", timevar="Period",
                        options=list(
                          height=400, 
                          width=1000, 
-                         state="{\"playDuration\":10000,
+                         state="{\"playDuration\":15000,
                          \"xZoomedDataMin\":0,
                          \"xZoomedIn\":false,
-                         \"time\":\"2010-07-01\",
+                         \"time\":\"2010-09-30\",
                          \"yZoomedIn\":false,
                          \"orderedByY\":false,
                          \"sizeOption\":\"4\",
                          \"xLambda\":1,
                          \"colorOption\":\"2\",
                          \"yZoomedDataMax\":130000,
-                         \"nonSelectedAlpha\":0.4,
+                         \"nonSelectedAlpha\":0.1,
                          \"iconType\":\"BUBBLE\",
                          \"dimensions\":{\"iconDimensions\":[\"dim0\"]},
-                         \"uniColorForNonSelected\":false,
-                       \"yZoomedDataMin\":0,
-                       \"xZoomedDataMax\":21,
-                       \"duration\":{\"multiplier\":1,\"timeUnit\":\"Q\"},
-                       \"xAxisOption\":\"3\",
-                       \"orderedByX\":true,
-                       \"showTrails\":false,
-                       \"yLambda\":1,
-                       \"yAxisOption\":\"2\",
-                       \"iconKeySettings\":[]};",
+                         \"uniColorForNonSelected\":true,
+                         \"yZoomedDataMin\":0,
+                         \"xZoomedDataMax\":21,
+                         \"duration\":{\"multiplier\":1,\"timeUnit\":\"Y\"},
+                         \"xAxisOption\":\"10\",
+                         \"orderedByX\":false,
+                         \"showTrails\":true,
+                         \"yLambda\":1,
+                         \"yAxisOption\":\"11\",
+                         \"iconKeySettings\":[]};",
                          showSelectListComponent = 1,
                          showHeader=1,
                          showAdvancedPanel=1,
@@ -68,7 +78,8 @@ Motion=gvisMotionChart(changel2, idvar="group", timevar="Period",
                          showSidePanel=1
                        ))
 
-print(Motion, "chart")
+plot(Motion)
+#print(Motion, "chart")
 
 # order levels
 
