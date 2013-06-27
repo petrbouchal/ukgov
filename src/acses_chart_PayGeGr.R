69eecfc0b64b37f0c2cbea1d51bb9d54267c8ab9
Petr Bouchal <pbouchal@gmail.com>
2013-06-27 18:14:30 +0100

    Charts for new cuts of data

---
+++ src/acses_chart_PayGeGr.R
@@ -0,0 +1,105 @@
+library(plyr)
+library(ggplot2)
+library(scales)
+library(grid)
+library(ggthemes)
+library(extrafont)
+
+# LOAD DATA
+#path  <- '/Users/petrbouchal/Downloads/ACSES/ACSES_Gender_Dept_Grade_Pay_data.tsv'
+path  <- 'P:/Research & Learning/Research/19. Transforming Whitehall/Whitehall Monitor/Data Sources/ONS Civil Service Statistics/Nomis ACSES/ACSES_Gender_Dept_Grade_Pay_data.tsv'
+acses <- read.delim(path, sep='\t')
+acses$value[acses$value=='#'] <- NA
+acses$value[acses$value=='..'] <- NA
+orgnames <- as.data.frame(unique(acses$new1))
+
+# FILTER
+org = 'Total (All Departments)'
+#ac_ch <- acses[acses$new1=='Total (All Departments)' & acses$Gender!='Total',]
+ac_ch <- acses[acses$new1==org &
+                 acses$Gender!='Total',]
+
+# turn value into numeric 'count' variable
+ac_ch$count <- as.numeric(as.character(ac_ch$value))
+
+# MERGE FILTER/GROUP DATA INTO MAIN DATA
+# ac_ch <- merge(ac_ch,orgs, all.x=TRUE)
+# ac_ch <- ac_ch[ac_ch$Include=='Yes',]
+# ac_ch <- unique(ac_ch) # removes duplicate lines for DfE and GEO
+
+# CREATE TOTALS PER GROUP
+totals <- ac_ch[ac_ch$Wage.band=='Total',]
+ac_ch <- ac_ch[ac_ch$Wage.band!='Total',]
+
+totals <- ddply(totals, .(Date, Civil.Service.grad), summarise,
+                  total=sum(count, na.rm=TRUE))
+
+# MERGE TOTALS INTO MAIN DATA
+ac_ch <- merge(ac_ch, totals, by=c('Civil.Service.grad','Date'))
+ac_ch$share <- ac_ch$count/ac_ch$total
+
+# Make female share negative
+ac_ch$share[ac_ch$Gender=='Female'] <- -ac_ch$share[ac_ch$Gender=='Female']
+ac_ch$count[ac_ch$Gender=='Female'] <- -ac_ch$count[ac_ch$Gender=='Female']
+
+# REORDER LEVELS
+ac_ch$Civil.Service.grad = factor(ac_ch$Civil.Service.grad,
+                                  levels(ac_ch$Civil.Service.grad)[c(1,2,4,5,3,6,7)])
+ac_ch$Wage.band = factor(ac_ch$Wage.band,
+                                  levels(ac_ch$Wage.band)[c(8,9,10,1:7)])
+# FILTER OUT UNNEDED BITS
+ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='Not reported',]
+ac_ch <- ac_ch[ac_ch$Wage.band!='not reported',]
+
+
+# CREATE GROUPINGS
+ac_ch$grp <- paste0(ac_ch$Civil.Service.grad, ac_ch$Gender) 
+
+# SELECT YEAR
+ac_ch <- ac_ch[ac_ch$Date==2012,]
+
+# PLOT
+#loadfonts()
+#loadfonts(device='win')
+#fonts()
+
+fontfamily = 'Calibri'
+plotname <- './graphs/ACSES graphs/plot_PayGrGe.pdf'
+plot_PayGrGe <- ggplot(ac_ch, aes(Wage.band, share)) +
+  geom_bar(position='identity', width=1, aes(fill=Gender),stat='identity') +
+#  geom_line(aes(group=grp, col=Gender), size=2) +
+#  geom_area(aes(group=grp, fill=Gender), data=ac_ch[ac_ch$Gender=='Female',]) +
+#  geom_area(aes(group=grp, fill=Gender), data=ac_ch[ac_ch$Gender=='Male',]) +
+#  geom_point(aes(col=Gender), pch=21, size=2) +
+#  geom_point(col='white', pch=19, size=1.5) +
+  coord_flip() +
+  scale_fill_manual(values=c('#d40072','#00ccff')) +
+  scale_colour_manual(values=c('#d40072','#00ccff')) +
+  guides(colour = guide_legend(ncol = 1)) +
+  guides(col=guide_legend(ncol=3)) +
+  theme_few() +
+  scale_y_continuous(labels=c('20%','0','20%'),
+                     limits=c(-max(abs(ac_ch$share),na.rm=TRUE),
+                              max(abs(ac_ch$share),na.rm=TRUE)),
+                     breaks=c(-.2,0,.2)) +
+  #scale_x_discrete(labels = c('AO','EO','SEO/HEO','G6/7','SCS')) +
+  theme(axis.text.x = element_text(angle = 0),
+        axis.text.y= element_text(vjust=0),
+        legend.title=element_blank(),
+        legend.position='bottom',
+        legend.direction='horizontal',
+        legend.key.size=unit(.4,units='cm'),
+        legend.text = element_text(vjust=1),
+        axis.ticks=element_blank(),
+        axis.title=element_blank(),
+        axis.text=element_text(colour='grey'),
+        panel.margin=unit(c(.1,.1,.1,.1),'cm'),
+        strip.text=element_text(face='bold'),
+        panel.border=element_rect(colour='grey'),
+        plot.title=element_text(family=fontfamily,face='bold',size=20,
+                                lineheight=2.5, vjust=2)) +
+  facet_wrap(~Civil.Service.grad, nrow=1) +
+  ggtitle('Civil Servants in Whitehall departments by grade and gender')
+plot_PayGrGe
+#ggsave(plotname, family=fontfamily, device=cairo_pdf)
+#embed_fonts(plotname, outfile=plotname)
