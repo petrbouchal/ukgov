2fd16f6cd254145cd46fac6ddfa4237461ef11db
Petr Bouchal <pbouchal@gmail.com>
2013-06-29 01:54:09 +0100

    New minority status charts added

--- src/acses_chart_DeGrMinYr.R
+++ src/acses_chart_DeGrMinYr.R
@@ -50,20 +50,23 @@ totals <- ddply(ac_ch, .(Group, Date, Civil.Service.grad), summarise,
 ac_ch <- merge(ac_ch, totals)
 ac_ch$share <- ac_ch$count/ac_ch$total
 
-# SELECT YEAR
-ac_ch <- ac_ch[ac_ch$Date=='2012' | ac_ch$Date=='2008',]
-
-# Reshape for calculation
-ac_ch <- melt(ac_ch, id.vars=c('Group','Date','Ethnic.grou','Civil.Service.grad'))
-ac_ch <- dcast(ac_ch, ... ~ variable + Date, drop=TRUE)
-ac_ch$sharediff <- (ac_ch$share_2012 - ac_ch$share_2008)
+# ADJUST FACTOR LABELS
+levels(ac_ch$Civil.Service.grad)[levels(ac_ch$Civil.Service.grad)=="Administrative officers and assistants"] <- "AO"
+levels(ac_ch$Civil.Service.grad)[levels(ac_ch$Civil.Service.grad)=="Executive officer"] <- "EO"
+levels(ac_ch$Civil.Service.grad)[levels(ac_ch$Civil.Service.grad)=="Senior and higher executive officer"] <- "SEO/HEO"
+levels(ac_ch$Civil.Service.grad)[levels(ac_ch$Civil.Service.grad)=="Senior Civil Service"] <- "SCS"
+levels(ac_ch$Civil.Service.grad)[levels(ac_ch$Civil.Service.grad)=="Total"] <- "All grades"
 
 # Filter out unneeded things
 ac_ch$Civil.Service.grad = factor(ac_ch$Civil.Service.grad,
                                   levels(ac_ch$Civil.Service.grad)[c(7,1,2,4,5,3,6)])
 ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='Not reported',]
+ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='AO',]
+ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='SEO/HEO',]
+ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='Grades 6 & 7',]
+ac_ch <- ac_ch[ac_ch$Civil.Service.grad!='EO',]
 ac_ch <- ac_ch[ac_ch$Ethnic.grou!='White',]
-ac_ch$grp <- paste0(ac_ch$Group, ac_ch$Gender)
+ac_ch$grp <- paste0(ac_ch$Group, ac_ch$Civil.Service.grad)
 
 # Build plot --------------------------------------------------------------
 
@@ -71,41 +74,41 @@ ac_ch$grp <- paste0(ac_ch$Group, ac_ch$Gender)
 #loadfonts(device='win')
 #fonts()
 
-plottitle='Share of minority Civil Servants in departments'
+plottitle='Share of minority Civil Servants in departments 2008-12, SCS and all'
 pheight = 6.5
 pwidth=9
 
 fontfamily = 'Calibri'
-plotname <- './charts/ACSES charts/plot_DeGrMin.pdf'
+plotname <- './charts/ACSES charts/plot_DeGrMinYr.pdf'
 ac_ch$alpha <- 1
-ac_ch$alpha[ac_ch$Civil.Service.grad=='Total'] <- .8
+ac_ch$minpop <- .14
 
-maxY = max(abs(ac_ch$share_2012),na.rm=TRUE)
+maxY = max(abs(ac_ch$share),na.rm=TRUE)
 
-plot_DeGrMin <- ggplot(ac_ch,aes(Civil.Service.grad, share_2012, alpha=alpha,
-                                 fill=sharediff)) +
-#  geom_line(aes(group=grp, col=Gender), size=2) +
+plot_DeGrMinYr <- ggplot(ac_ch,aes(as.factor(Date), share, group=grp)) +
+  geom_line(aes(y=minpop, group=grp, col='grey'), size=.5) +
+  geom_line(aes(group=grp, col=Civil.Service.grad), size=1.5) +
 #  geom_area(aes(group=grp, fill=Gender), data=ac_ch[ac_ch$Gender=='Female',]) +
 #  geom_area(aes(group=grp, fill=Gender), data=ac_ch[ac_ch$Gender=='Male',]) +
 #  geom_point(aes(col=Gender), pch=21, size=2) +
-  geom_bar(position='identity', width=.6,stat='identity',colour='#00ccff', fill='#00ccff') +
-  geom_segment(aes(y=share_2008, yend=share_2012, xend=Civil.Service.grad, alpha=1),
-               colour='#d40072', size=1,
-               arrow=arrow(length=unit(.1,'cm'),type='closed')) +
+#  geom_bar(position='identity', width=.6,stat='identity',colour='#00ccff', fill='#00ccff') +
+#  geom_segment(aes(y=share_2008, yend=share_2012, xend=Civil.Service.grad, alpha=1),
+#               colour='#d40072', size=1,
+#               arrow=arrow(length=unit(.1,'cm'),type='closed')) +
   #geom_point(col='#d40072', pch=18, size=2.5, aes(alpha=1)) +
-  coord_flip() +
-  #scale_colour_manual(values=c('#d40072','#00ccff')) +
+#  coord_flip() +
+  scale_colour_manual(values=c('#d40072','grey','#00ccff')) +
   guides(colour = guide_legend(ncol = 1)) +
   guides(col=guide_legend(ncol=3)) +
   theme_few() +
   scale_y_continuous(breaks=c(0,.25,.5),
-                     limits=c(0,maxY),
+                     limits=c(0,1),
                      labels=c('0','25%','50%')) +
-  scale_x_discrete(labels = c('All grades','AO','EO','SEO/HEO','G6/7','SCS')) +
+  #scale_x_discrete(labels = c('All grades','AO','EO','SEO/HEO','G6/7','SCS')) +
   theme(axis.text.x = element_text(angle = 0),
         axis.text.y= element_text(vjust=0),
         legend.title=element_blank(),
-        legend.position='none',
+        legend.position='bottom',
         legend.direction='horizontal',
         legend.key.size=unit(.4,units='cm'),
         legend.text = element_text(vjust=1),
@@ -120,12 +123,9 @@ plot_DeGrMin <- ggplot(ac_ch,aes(Civil.Service.grad, share_2012, alpha=alpha,
   facet_wrap(~Group, nrow=3) +
   ggtitle(plottitle)
 
-
 # Draw plot ---------------------------------------------------------------
 
-
-plot_DeGrMin
-
+plot_DeGrMinYr
 
 # Save plot ---------------------------------------------------------------
 
