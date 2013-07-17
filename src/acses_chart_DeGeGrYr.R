9b88ea4153745d89428a540d5f453e7f113501b7
Petr Bouchal <pbouchal@gmail.com>
2013-07-17 01:01:16 +0100

    Finished & sorted gender SCS&All chart by dept

--- src/acses_chart_DeGeGrYr.R
+++ src/acses_chart_DeGeGrYr.R
@@ -11,15 +11,12 @@ uu <- origdata
 # FILTER OUT WAGE BAND LINES
 uu <- uu[uu$Wage.band=='Total',]
 uu <- AddOrgData(uu)
-
-# MERGE FILTER/GROUP DATA INTO MAIN DATA
+totals <- uu[uu$Gender=='Total',]
 uu <- uu[uu$Gender!='Total',]
 
 # CREATE TOTALS PER GROUP
 uu <- ddply(uu, .(Group, Gender, Date, Civil.Service.grad),
                summarise, count=sum(count, na.rm=TRUE))
-totals <- uu[uu$Civil.Service.grad=='Total',]
-uu <- uu[uu$Civil.Service.grad!='Total',]
 
 totals <- ddply(totals, .(Group, Date,Civil.Service.grad), summarise,
                   total=sum(count, na.rm=TRUE))
@@ -28,34 +25,23 @@ totals <- ddply(totals, .(Group, Date,Civil.Service.grad), summarise,
 uu <- merge(uu, totals)
 uu$share <- uu$count/uu$total
 
-# SELECT GRADES
-uu <- uu[uu$Civil.Service.grad=='SCS' | uu$Civil.Service.grad=='All grades',]
-uu$grp <- paste0(uu$Group, uu$Gender) 
-
 uu <- RelabelGrades(uu)
 
-# Sort departments --------------------------------------------------------
-gradevalues <- data.frame('gradeval'=c(1:length(levels(uu$Civil.Service.grad))),
-                          'Civil.Service.grad'=levels(uu$Civil.Service.grad))
-uu <- merge(uu,gradevalues)
-xtot <- ddply(uu[uu$Date==2012],.(Group, Civil.Service.grad),
-              summarise,sharebothgenders=sum(share, na.rm=TRUE))
-uu <- merge(uu,xtot)
-uu <- merge(uu,gradevalues)
-uu$gradescore <- uu$gradeval*uu$sharebothgenders
-xtot <- ddply(uu,.(Group,Date),summarise,meangradescore=mean(gradescore))
-uu <- merge(uu,xtot)
-uu$sorter <- uu$meangradescore
-#make Whole CS category go last
-uu$sorter[uu$Group=='Whole Civil Service'] <- max(uu$sorter)*1.1
-#reorder grouping variable
-uu$Group <- reorder(uu$Group,uu$sorter,mean)
+# SELECT DATA
+uu <- uu[uu$Civil.Service.grad=='SCS' | uu$Civil.Service.grad=='All grades',]
+uu$grp <- paste0(uu$Group, uu$Civil.Service.grad) 
+uu <- uu[uu$Gender=='Female',]
 
+# Sort departments --------------------------------------------------------
+
+xtot <- ddply(uu[uu$Date==2012,],.(Group),summarise,sorter=sum(share))
+uu <- merge(uu,xtot,all.x=T)
+#make Whole CS category go last
+uu$sorter[uu$Group=='Whole Civil Service'] <- min(uu$sorter)/1.1
+#reorder grouping variable
+uu$Group <- reorder(uu$Group,-uu$sorter)
 
 # Build plot --------------------------------------------------------------
-# Make female share negative
-uu$share[uu$Gender=='Female'] <- -uu$share[uu$Gender=='Female']
-uu$count[uu$Gender=='Female'] <- -uu$count[uu$Gender=='Female']
 
 plotformat='eps'
 plotname <- 'plot_DeGeGrYr'
@@ -68,17 +54,14 @@ pw=24.5
 uu$yvar <- uu$share
 
 maxY <- max(abs(uu$yvar),na.rm=TRUE)
-ylimits <- c(-maxY*1.04, maxY*1.04)
-ybreaks <- c(-.3,-.15,0,.15,.3)
+ylimits <- c(0, maxY*1.04)
+ybreaks <- c(0,.2,.4,.6)
 ylabels <- paste0(abs(ybreaks*100),'%')
 
-plot_DeGeGrYr <- ggplot(uu, aes(Civil.Service.grad, share)) +
-  geom_bar(position='identity', width=1, aes(fill=Gender),stat='identity') +
-  coord_flip() +
-  scale_fill_manual(values=c(IfGcols[3,1],IfGcols[2,1]),
-                    labels=c('Female   ', 'Male')) +
-  guides(colour = guide_legend(ncol = 1)) +
-  guides(col=guide_legend(ncol=3)) +
+plot_DeGeGrYr <- ggplot(uu, aes(as.factor(Date), share,group=grp)) +
+  geom_line(size=.6, aes(colour=Civil.Service.grad),stat='identity') +
+  scale_colour_manual(values=c('All grades' = IfGcols[2,1],'SCS'=IfGcols[3,1])) +
+  guides(colour = guide_legend(ncol = 2)) +
   scale_y_continuous(breaks=ybreaks,limits=ylimits,labels=ylabels) +
   facet_wrap(~Group, nrow=3) +
   labs(title=plottitle, y=ylabel,x=xlabel) +
