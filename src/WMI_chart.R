514664e94053fd0a3f65e4523a567e00789924b9
Petr Bouchal <pbouchal@gmail.com>
2013-07-18 19:26:32 +0100

    WMI month format

--- src/WMI_chart.R
+++ src/WMI_chart.R
@@ -7,10 +7,12 @@ uu$Paybill_Payroll <- uu$Paybill_Payroll/10E3
 
 uu <- melt(uu)
 uu$grp <- paste0(uu$variable,uu$Dept)
+uu$Month <- as.Date(uu$Month,format='%d/%m/%Y')
 uu$Month <- as.POSIXct(uu$Month)
 
 wmiplot <- ggplot(uu,aes(x=Month,y=value)) +
   geom_line(aes(colour=variable,group=grp)) +
   scale_x_datetime(labels=date_format('%Y-%m')) +
+  #scale_colour_manual(values=c(IfGcols[1,1],IfGcols[2,1]))
   facet_wrap(~Dept,scales='free_y',nrow=7)
 wmiplot
