705505db3bef046b80483fd03f3d9633303acb59
Petr Bouchal <pbouchal@gmail.com>
2013-07-24 19:11:21 +0100

    WMI chart improved

--- src/WMI_chart.R
+++ src/WMI_chart.R
@@ -1,9 +1,12 @@
 source('./src/acses_lib.R')
 
-origdata <- read.csv('./data-input/WMI.csv')
 
+# Load data ---------------------------------------------------------------
+
+origdata <- read.csv('./data-input/WMI.csv')
 uu <- origdata
-uu$Paybill_Payroll <- uu$Paybill_Payroll/10E3
+
+# Process data ------------------------------------------------------------
 
 uu <- melt(uu)
 uu$grp <- paste0(uu$variable,uu$Dept)
@@ -16,25 +19,39 @@ wmibase <- data.frame('Dept'=uu$Dept[uu$Month==min(uu$Month)],
 uu <- merge(uu,wmibase)
 uu$valuepc <- uu$value/uu$base-1
 
+
+# Filter
+uu <- uu[uu$Dept!='W Office',]
+uu <- uu[uu$Dept!='S Office',]
+uu <- uu[uu$Dept!='AG Depts',]
+
+# Setup plot --------------------------------------------------------------
+
 pw <- 24.5
-ph <- 15.3
+ph <- 15.3-2.5
 
 uu$yvar <- uu$valuepc
 plottitle <- ''
 plotname <- 'WMI_depts'
-plotformat <- 'emf'
+plotformat <- 'eps'
 ylabel <- 'Pay bill (£000\'s)/Staff count (FTE)'
 xlabel <- NULL
 
+ylimits <- c(min(uu$yvar,na.rm=TRUE),max(uu$yvar,na.rm=TRUE))
+
+# Build plot --------------------------------------------------------------
+
 wmiplot <- ggplot(uu,aes(x=Month,y=yvar)) +
   geom_line(aes(colour=variable,group=grp),size=1) +
+  geom_hline(y=0, colour=IfGcols[1,2]) +
   scale_x_datetime(labels=date_format('%Y-%m')) +
   scale_colour_manual(values=c(IfGcols[3,1],IfGcols[2,1]),
-                      labels=c('FTE (Payroll)','Pay bill (Payroll)'))+
-  scale_y_continuous(labels=percent) +
-  facet_wrap(~Dept,scales='free_y',ncol=4) +
+                      labels=c('FTE (Payroll)','Pay bill (Payroll)')) +
+  scale_y_continuous(labels=percent,limits=ylimits) +
+  facet_wrap(~Dept) +
   theme(panel.border=element_rect(fill=NA,colour=IfGcols[1,3]),
-        plot.title=element_blank()) +
+        plot.title=element_blank(), axis.ticks=element_line(colour=IfGcols[1,3]),
+        axis.ticks.y=element_blank()) +
   labs(x=xlabel,y=ylabel)
 wmiplot
 
