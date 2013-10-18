f671d0a577a1d00c92a773ccd32629efd16a2b59
Petr Bouchal <pbouchal@gmail.com>
2013-10-18 18:24:32 +0100

    Started code for checking orgs in 2013 data

---
+++ src/acses/acses_checking2013.R
@@ -0,0 +1,7 @@
+source('./src/lib/lib_acses.R')
+
+# Load sample data --------------------------------------------------------
+
+
+# Check if all organisations have groupings against them ------------------
+
