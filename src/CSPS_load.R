e5cb6be6d8deee880361f6a0260b39b3a044391d
Petr Bouchal <pbouchal@gmail.com>
2014-02-28 00:30:45 +0000

    Improved rCharts sandbox + updated CSPS script

--- src/CSPS_load.R
+++ src/CSPS_load.R
@@ -1,4 +1,5 @@
 source('./src/lib/lib_acses.R')
+library(stringr)
 
 read.tcsv = function(file, header=TRUE, sep=",", ...) {
   # from http://stackoverflow.com/questions/17288197/reading-a-csv-file-organized-horizontally
@@ -19,5 +20,22 @@ read.tcsv = function(file, header=TRUE, sep=",", ...) {
 }
 
 path=paste0(whmdatafolder,'Civil Service People Survey')
-path='P:/Research & Learning/Research/19. Transforming Whitehall/Whitehall Monitor/Data Sources/Civil Service People Survey/CSPS 2013 - Benchmark/csps2013_demographic_results_transposed.csv'
-psdet <- read.csv(path)
+path='./data-input/csps2013_demographic_results_transposed.csv'
+path='http://resources.civilservice.gov.uk/wp-content/uploads/2014/02/csps2013_demographic_results.csv'
+psdet <- as.data.frame(t(read.csv(path,header=F)))
+
+headers <- psdet[1,]
+headers <-  str_replace_all(headers,'\\[','')
+headers <-  str_replace_all(headers,'\\]','')
+headers <-  str_replace_all(headers,'\\(','')
+headers <-  str_replace_all(headers,'\\)','')
+headers <-  str_replace_all(headers,'\\=','xx')
+headers <-  str_replace_all(headers,'\\,','yy')
+headers <-  str_replace_all(headers,'/','')
+headers <-  str_replace_all(headers,' : ','')
+headers <-  str_replace_all(headers,'\\:','')
+headers <-  str_replace_all(headers,'-:','.')
+# headers <-  str_replace_all(headers,'[^ ]*','.')
+headers
+names(psdet) <- headers
+str(psdet)
--- src/rCharts_sandbox.R
+++ src/rCharts_sandbox.R
@@ -1,6 +1,6 @@
 # Setup -------------------------------------------------------------------
-require(devtools)
-install_github('rCharts', 'ramnathv')
+# require(devtools)
+# install_github('rCharts', 'ramnathv')
 
 require('rCharts')
 source('./src/lib/lib_acses.R')
@@ -8,9 +8,14 @@ source('./src/pse/PSE_Reshape.R') # to prepare data if needed
 uu <- change
 
 uu2 <- uu[uu$measure=='Cumulative_Perc_net_change',]
-# rr <- xPlot(value ~ Period | Dept, color='Whitehall', data=uu2, type='line-dotted',group=c('Dept','Whitehall')
+uu2 <- uu2[uu$Whitehall!='Total Employment ONS Total',]
+uu2 <- uu2[uu$Period=='2013Q3',]
+uu2 <- uu2[complete.cases(uu2),]
+rr <- xPlot(value ~ Dept | Whitehall, color='Whitehall', data=uu2, type='line-dotted',group=c('Dept','Whitehall'))
 # rr <- xPlot(value ~ Period, color='Whitehall', data=uu2, type='line-dotted',group=c('Dept','Whitehall'))
-rr <- nPlot(value ~ Period | Dept, color='Whitehall', data=uu2, type='multiBarChart',group=c('Dept','Whitehall'))
+rr <- rPlot(value ~ Dept, color='Whitehall', data=uu2, type='multiBarChart', group='group')
+rr <- nPlot(value ~ Dept, data=uu2, type='multiBarChart',group='Whitehall')
 
-rr$params$height <- 800
+rr$params$height <- 600
+rr$params$width <- 800
 rr
