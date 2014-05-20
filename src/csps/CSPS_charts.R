7e409130b91e06270edcb64a88af4ea0888e9589
Petr Bouchal <pbouchal@gmail.com>
2014-05-20 01:13:10 +0100

    Complete processing of CSPS demographics + first chart code

---
+++ src/csps/CSPS_charts.R
@@ -0,0 +1,15 @@
+library(pbtools)
+
+csps <- read.csv('./data-output/CSPS_demographic_long.csv')
+
+csps2 <- csps[csps$eng_score_component & csps$dimension=='Time in current job',]
+plot <- ggplot(data=csps2,aes(x=split,y=value)) +
+  geom_bar(position='dodge',stat='identity',fill=themecols[1,1]) +
+  scale_y_continuous(labels=percent,breaks=c(0,.25,.5,.75,1),limits=c(0,1)) +
+  facet_wrap(~measure,nrow = 5) +
+  coord_flip() +
+  LoadCustomThemes(themecols,'Verdana') +
+  theme(panel.grid.major.y=element_blank(),
+        panel.grid.major.x=element_line(),axis.title=element_blank())
+plot
+  
--- src/csps/CSPS_load.R
+++ src/csps/CSPS_load.R
@@ -1,28 +1,9 @@
 # Loads Civil Service People Survey detailed dataset
 
-# source('./src/lib/lib_acses.R')
 library(pbtools)
 library(zoo)
 
 whmdatafolder <- 'P:/Research & Learning/Research/19. Transforming Whitehall/Whitehall Monitor/Data Sources/'
-
-read.tcsv = function(file, header=TRUE, sep=",", ...) {
-  # from http://stackoverflow.com/questions/17288197/reading-a-csv-file-organized-horizontally
-  n = max(count.fields(file, sep=","), na.rm=TRUE)
-  x = readLines(file)
-  
-  .splitvar = function(x, sep, n) {
-    var = unlist(strsplit(x, split=sep))
-    length(var) = n
-    return(var)
-  }
-  
-  x = do.call(cbind, lapply(x, .splitvar, sep=sep, n=n))
-  x = apply(x, 1, paste, collapse=sep) 
-  out = read.csv(text=x, sep=sep, header=header, ...)
-  return(out)
-  
-}
 
 ## Load data
 path=paste0(whmdatafolder,'Civil Service People Survey')
@@ -34,30 +15,25 @@ psdet[,87] <- NULL # remove empty last column
 
 ## Clean up variable names
 headers <- t(psdet[1,])
-headers <-  str_replace_all(headers,'\\[','')
-headers <-  str_replace_all(headers,'\\]','')
-headers <-  str_replace_all(headers,'\\(','')
-headers <-  str_replace_all(headers,'\\)','')
-headers <-  str_replace_all(headers,'\\=','xx')
-headers <-  str_replace_all(headers,'\\,','yy')
-headers <-  str_replace_all(headers,'/','')
-headers <-  str_replace_all(headers,' : ','')
-headers <-  str_replace_all(headers,'\\:','')
 headers <-  str_replace_all(headers,'-:','.')
-headers <-  str_replace_all(headers,' $','')
+headers <-  str_trim(headers)
 
 ## Turn variable names into codebook and usable column names
-codebookQ <- str_split_fixed(headers,'\\. ',2)
-# headers <-  str_replace_all(headers,'[^ ]*','.')
-newheaders <- str_replace_all(codebookQ[,1],' ','.')
-names(psdet) <- tolower(newheaders)
+codebookQ <- as.data.frame(str_split_fixed(headers,'\\. ',2))
+codebookQ$newheaders <- tolower(str_replace_all(codebookQ[,1],' ','.'))
+names(codebookQ) <- c('measure','questiontext','mergevar')
+names(psdet) <- tolower(codebookQ$mergevar)
+
 rownames(psdet) <- NULL
 psdet <- psdet[-1,] # remove first row which contains row names
 names(psdet)
 
+qnames <- cbind()
+
 ## Split measure variable
-psdet$dimension <- str_split_fixed(psdet$measure,':',2)[,1]
-psdet$split <- str_split_fixed(psdet$measure,':',2)[,2]
+splitmeasure <- str_split_fixed(psdet$measure,':',2)
+psdet$dimension <- splitmeasure[,1]
+psdet$split <- splitmeasure[,2]
 
 psdet$dimension[psdet$dimension==''] <- NA
 psdet$dimension <- na.locf(psdet$dimension)
@@ -68,7 +44,30 @@ psdetl <- melt(psdet,id.vars = c('dimension','split'))
 psdetl$value <- as.numeric(str_replace(psdetl$value,'%',''))/100 # fix %
 
 ## Add question text from codebook
-codebookdf <- as.data.frame(codebookQ)
-codebookdf$V1 <- tolower(codebookdf$V1)
-psdetl <- merge(psdetl,codebookdf,by.x = 'variable', by.y='V1')
-rename(psdetl$V2,replace = 'questiontext')
+psdetl <- merge(psdetl,codebookQ,by.x = 'variable', by.y='mergevar',all.x=T)
+
+## Clean up split variable
+
+# Create split N variable
+splitsplit <- str_split_fixed(psdetl$split,'\\(n=',n=2)
+psdetl$split <- str_trim(splitsplit[,1])
+
+split_n <- str_replace(splitsplit[,2],'\\)','')
+split_n <- str_replace(split_n,',','')
+split_n <- str_trim(split_n)
+psdetl$split_n <- as.numeric(split_n)
+
+# Create dimension code variable
+dimsplit <- str_split_fixed(psdetl$dimension,'\\[',n=2)
+psdetl$dimension <- str_trim(dimsplit[,1])
+
+dim_id <- str_replace(dimsplit[,2],'\\]','')
+psdetl$dim_id <- str_trim(dim_id)
+
+## Mark engagement score components
+psdetl$eng_score_component <- FALSE
+psdetl$eng_score_component[str_detect(psdetl$variable,'\\.')] <- TRUE
+
+## Write to CSV
+write.csv(psdetl,'./data-output/CSPS_demographic_long.csv',row.names=F)
+
