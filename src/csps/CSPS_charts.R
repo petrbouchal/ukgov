2f3d40741a6e993ca0cb95b7d945bd74d00dd0a8
Petr Bouchal <pbouchal@gmail.com>
2014-05-23 00:58:44 +0100

    Chart style tweaks

--- src/csps/CSPS_charts.R
+++ src/csps/CSPS_charts.R
@@ -1,15 +1,17 @@
 library(pbtools)
+library(devtools)
+# install_github('pbtools','petrbouchal')
 
 csps <- read.csv('./data-output/CSPS_demographic_long.csv')
 
 csps2 <- csps[csps$eng_score_component & csps$dimension=='Time in current job',]
 plot <- ggplot(data=csps2,aes(x=split,y=value)) +
-  geom_bar(position='dodge',stat='identity',fill=themecols[1,1]) +
+  geom_bar(position='dodge',stat='identity',fill=themecols[4,1]) +
   scale_y_continuous(labels=percent,breaks=c(0,.25,.5,.75,1),limits=c(0,1)) +
   facet_wrap(~measure,nrow = 5) +
   coord_flip() +
-  LoadCustomThemes(themecols,'Verdana') +
+  LoadCustomThemes(themecols[,1],'Verdana') +
   theme(panel.grid.major.y=element_blank(),
-        panel.grid.major.x=element_line(),axis.title=element_blank())
+        panel.grid.major.x=element_line())
 plot
   
