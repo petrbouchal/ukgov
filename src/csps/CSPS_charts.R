49204cb3f6b44e0a8b78206d90fd35aef8f8098a
Petr Bouchal <pbouchal@gmail.com>
2014-05-22 19:11:52 +0100

    Minor tweaks

--- src/csps/CSPS_charts.R
+++ src/csps/CSPS_charts.R
@@ -1,6 +1,6 @@
 library(pbtools)
 
-csps <- read.csv('./data-output/CSPS_demographic_long.csv')
+csps <- fread('./data-output/CSPS_demographic_long.csv')
 
 csps2 <- csps[csps$eng_score_component & csps$dimension=='Time in current job',]
 plot <- ggplot(data=csps2,aes(x=split,y=value)) +
@@ -8,8 +8,8 @@ plot <- ggplot(data=csps2,aes(x=split,y=value)) +
   scale_y_continuous(labels=percent,breaks=c(0,.25,.5,.75,1),limits=c(0,1)) +
   facet_wrap(~measure,nrow = 5) +
   coord_flip() +
-  LoadCustomThemes(themecols,'Verdana') +
+  LoadCustomThemes(themecols[,1],'Verdana') +
   theme(panel.grid.major.y=element_blank(),
         panel.grid.major.x=element_line(),axis.title=element_blank())
 plot
-  
+SavePlot
