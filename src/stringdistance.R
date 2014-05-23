04fe792df448dba72e1179ec401c7f7a469fc931
Petr Bouchal <pbouchal@gmail.com>
2014-05-23 18:44:32 +0100

    update to string distance code

--- src/stringdistance.R
+++ src/stringdistance.R
@@ -1,5 +1,6 @@
 library(pbtools)
 library(stringdist)
+library(gdata)
 
 mpnames <- fread('./data-input/majorprojectnames.csv',na.strings = '')
 cmpnames <- expand.grid(mpnames)
@@ -9,10 +10,15 @@ cmpnames <- cmpnames[complete.cases(cmpnames),]
 cmpnames <- sort_df(cmpnames,'dist')
 
 cmpnames2012 <- group_by(cmpnames,'Projects2012') %>%
-  summarise(lowestdist=min(dist))
+  summarise(lowestdist=min(dist)) %>%
+  do(rename.vars(.,from='Projects2012',to='projectnames'))
 table(cmpnames2012$lowestdist)
 
 cmpnames2013 <- group_by(cmpnames,'Projects2013') %>%
-  summarise(lowestdist=min(dist))
+  summarise(lowestdist=min(dist)) %>%
+  do(rename.vars(.,from='Projects2013',to='projectnames'))
+  
 table(cmpnames2013$lowestdist)
   
+all <- merge(cmpnames2012,cmpnames2013,all=T)
+all <- sort_df(all,'lowestdist')
