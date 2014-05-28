11b386017e1b7957a4f754673d6d151a475cdf4a
Petr Bouchal <pbouchal@gmail.com>
2014-05-29 00:04:19 +0100

    Improved processing of Gov.uk publications + data file

--- src/govuk_publication.R
+++ src/govuk_publication.R
@@ -3,23 +3,21 @@
 
 library(rjson)
 library(pbtools)
-library(lubridate)
+library(RCurl)
 
-LoadCustomThemes(mycols=ifgbasecolours,fontfamily = 'Calibri')
+LoadCustomThemes(mycols=ifgbasecolours[,1],fontfamily = 'Calibri',tints = c(0.75,0.5,0.25))
 
 # load pages and create a long list of all results rows (list of lists)
 results <- list()
+temporaryFile <- tempfile()
 for (i in 1:500) {
   url <- paste0('https://www.gov.uk/government/publications.json?page=',i)
-  pubsfile <- file(url)
-  pubs <- readLines(pubsfile)
+  pubsfile <- download.file(url,destfile = temporaryFile, method='curl',quiet = T)
+  pubs <- readLines(temporaryFile)
   pubsj <- fromJSON(pubs)
   results <- append(results, pubsj$results)
-  close(pubsfile)
-  print(i)
+  if (i%%10==0) {print(i)}
 }
-
-str(results)
 
 # turn the list of lists into a data frame
 # first fix NULL values
@@ -35,29 +33,55 @@ df <- as.data.frame(do.call("rbind", json_file))
 df$date <- strptime(df$public_timestamp, format='%Y-%m-%dT%H:%M:%S')
 df$dayname <- wday(df$date,label = T)
 df$daynum <- wday(df$date,label = F)
-df$hour <- hour(df$date)
+df$hour <- sprintf('%02s',hour(df$date))
 df$weekid <- paste0(week(df$date),'_',year(df$date))
 df$dayhour <- paste0(df$daynum, '_', df$hour)
-df$date <- NULL
+
+
+# dig out organisation from HTML - TODO
+# df$orgname <- str_extract(df$organisations,)
 
 # create aggregate variables and filter out what's not needed
-dfs <- group_by(df, weekid, display_type) %>%
-  mutate(a=n(), aa=sum(a)) %>%
-  ungroup() %>%
-  group_by(weekid, hour, display_type) %>%
-  mutate(b=n(),
-         c=b/aa) %>%
-  filter(display_type=='Transparency data')
+dfs <- select(df, dayname, hour, dayhour, display_type, weekid) %>%
+  mutate(count_all=n()) %>% # count of everything in the dataset
+  group_by(dayhour) %>%
+  mutate(count_dayhour=n(),share_dayhour=n()/count_all) %>% # rate of evrthng by hour
+  ungroup() %>%  
+  group_by(display_type) %>%
+  mutate(counttype=n()) %>% # count by type
+  group_by(dayname, hour, dayhour, display_type) %>% 
+  mutate(sharedayhourtype=n()/counttype) %>% # rate by type
+  summarise(share_type=mean(sharedayhourtype),count_type=n(), # means
+            count_all=mean(count_all),share_all=mean(share_dayhour)) %>%
+  filter(display_type=='Transparency data') %>%
+  melt() %>% # reshape
+#   filter(variable=='share_type' | variable=='share_all') %>%
+  filter(variable=='share_type') %>%
+  mutate(display_type=as.character(display_type), # reformulate labels
+         display_type=ifelse(variable=='share_all','All publications',display_type))
+
 
 # plot
-plot <- ggplot(dfs, aes(x=hour, y=..count..)) +
-  geom_bar(stat="bin",position = 'stack') +
-  facet_wrap(~day, scales = 'free_x') +
-  scale_colour_discrete(breaks=ifgcolours)
+
+plot <- ggplot(dfs, aes(x=hour, y=value, fill=dayname)) +
+  geom_bar(stat="identity",position = 'stack') +
+  scale_fill_manual(values=ifgbasecolours) +
+  facet_wrap(~dayname, nrow=1)
 plot
 
-plot <- ggplot(dfs, aes(x=dayhour, y=c, fill=dayname)) +
-  geom_bar(stat="identity",position = 'stack') +
-  scale_fill_manual(values=ifgcolours)
+plot <- ggplot(dfs, aes(x=hour, y=value, fill=display_type, group=display_type)) +
+  geom_bar(stat="identity",position='dodge') +
+  scale_fill_manual(values=ifgbasecolours) +
+  facet_wrap(~dayname, nrow=1) 
 plot
+
+plot <- ggplot(dfs, aes(x=dayname, y=value, fill=dayname)) +
+  geom_bar(stat="identity",position = 'stack') +
+  scale_fill_manual(values=ifgbasecolours)
+plot
+
+sum(dfs$value[as.numeric(dfs$hour)<15 & dfs$dayname!='Fri'])
+mean(dfs$value[as.numeric(dfs$hour)<15 & dfs$dayname!='Fri'])
+sum(dfs$value[as.numeric(dfs$hour)>14 & dfs$dayname=='Fri'])
+mean(dfs$value[as.numeric(dfs$hour)>14 & dfs$dayname=='Fri'])
 
