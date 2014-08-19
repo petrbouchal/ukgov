53655037ab8a9b8e774c7715cae2725cc0072413
Petr Bouchal <pbouchal@gmail.com>
2014-08-19 19:34:20 +0100

    update DeGeGr chart

--- src/acses/acses_chart_DeGeGr_new.R
+++ src/acses/acses_chart_DeGeGr_new.R
@@ -1,3 +1,5 @@
+library(plyr)
+library(pbtools)
 source('./src/lib/lib_acses.R')
 
 # Load data ---------------------------------------------------------------
@@ -8,15 +10,22 @@ whitehallonly <- TRUE
 
 # Process data ------------------------------------------------------------
 uu <- origdata %>%
-  filter(Wage.band=='Total' & Gender!='Total' & Wage.band=='Total') %>%
+  # Filter out unneeded totals
+  filter(Wage.band=='Total' & Gender!='Total') %>%
+  # Add organisation data and exclude what isn't needed
   AddOrgData(whitehallonly) %>%
   filter(Include=='Yes') %>%
+  # Drop unneeded vars
   select(Group, Whitehall, Gender, Civil.Service.grad, Date, count, Organisation) %>%
+  # Summarise by departmental group
   group_by(Group, Whitehall, Gender, Date, Civil.Service.grad) %>%
   summarise(count=sum(count, na.rm=T)) %>%
+  # Create total variable - dept group total on each row
   group_by(Group, Whitehall, Date) %>%
   mutate(total=sum(count[Civil.Service.grad=='Total'])) %>%
+  # Exclude unneeded grades
   filter(Civil.Service.grad!='Total' & Civil.Service.grad!='Not reported') %>%
+  # create share variable
   mutate(share=count/total, grp = paste0(Group, Gender)) %>%
   RelabelGrades()
   
@@ -45,7 +54,7 @@ uu <- merge(uu,gradevalues) %>%
 if(whitehallonly) {
   uu$Group <- revalue(uu$Group,c("Whole Civil Service"="Whitehall"))
 }
-HLcol <- ifelse(whitehallonly,IfGcols[4,1],IfGcols[3,1])
+HLcol <- ifelse(whitehallonly,ifgcolours[4,1],ifgcolours[3,1])
 
 plotname <- 'plot_DeGeGr'
 plottitle <- 'Civil Servants by gender and grade'
@@ -62,26 +71,27 @@ if(whitehallonly){
 
 uu$yvar <- uu$share
 
+
 maxY <- max(abs(uu$yvar),na.rm=TRUE)
 ylimits <- c(-maxY*1.04, maxY*1.04)
 ybreaks <- c(-.3,-.15,0,.15,.3)
 ylabels <- paste0(abs(ybreaks*100),'%')
 
+loadcustomthemes(ifgcolours, 'Calibri')
 plot_DeGeGr <- ggplot(uu, aes(Civil.Service.grad, share)) +
   geom_rect(data = uu[uu$totalgroup,],fill=HLcol,xmin = -Inf,xmax = Inf,
             ymin = -Inf,ymax = Inf,alpha = .01) +
-  geom_bar(position='identity', width=1, aes(fill=Gender),stat='identity') +
   geom_rect(data = uu[uu$totalgroup,],colour=HLcol,xmin = -Inf,xmax = Inf,
             ymin = -Inf,ymax = Inf,alpha = 1,fill=NA,size=2) +
+  geom_bar(position='identity', width=1, aes(fill=Gender),stat='identity') +
   coord_flip() +
-  scale_fill_manual(values=c(IfGcols[2,1],IfGcols[5,1]),
-                    labels=c('Female   ', 'Male')) +
-  guides(colour = guide_legend(ncol = 1)) +
-  guides(col=guide_legend(ncol=3)) +
-  scale_y_continuous(breaks=ybreaks,limits=ylimits,labels=ylabels) +
   facet_wrap(~Group, nrow=3) +
+  scale_fill_manual(values=c(ifgcolours[c(2,5),1]),
+                    labels=c('Female   ', 'Male')) +
+  guides(fill=guide_legend(ncol=3)) +
+  scale_y_continuous(breaks=ybreaks,limits=ylimits,labels=ylabels) +
   labs(y=ylabel) +
-  theme(panel.border=element_rect(fill=NA,color=IfGcols[1,3]),
+  theme(panel.border=element_rect(fill=NA,color=ifgcolours[1,3]),
         plot.title=element_blank(),axis.title.y=element_blank(),
         panel.grid.major.x=element_line(), axis.ticks=element_blank(),
         panel.grid.major.y=element_blank())
@@ -89,4 +99,5 @@ plot_DeGeGr
 
 # Save plot ---------------------------------------------------------------
 
-# SavePlot(plotname=plotname,plotformat=plotformat,ploth=ph,plotw=pw,ffamily=fontfamily)
+saveplot(plotname=plotname,plotformat=plotformat,ploth=ph,plotw=pw,ffamily=fontfamily,
+         plotdir='./charts-output/')
