ff144bf36edfa55b6300b999c95e07015136b8a1
Petr Bouchal <pbouchal@gmail.com>
2014-08-19 19:33:29 +0100

    improve acses library

--- src/lib/lib_acses.R
+++ src/lib/lib_acses.R
@@ -1,9 +1,9 @@
-source('./src/lib/load_packages.R')
+# source('./src/lib/load_packages.R')
 
 whmdatafolder <- 'P:/Research & Learning/Research/19. Transforming Whitehall/Whitehall Monitor/Data Sources/'
 
 # Set location ------------------------------------------------------------
-if(Sys.info()[['sysname']]=='Darwin') {location='home'} else {location='ifg'}
+if(whatplatform()=='Darwin') {location='home'} else {location='ifg'}
 
 # set parameters
 if(!exists('batchproduce')) {batchproduce <- FALSE}
@@ -24,11 +24,12 @@ if(location=='ifg') {
   loadfonts(device='win',quiet=TRUE)
 }
 
-source('./src/lib/GetColorTable.R')
-source('./src/lib/TintShade.R')
-source('./src/lib/SortGroups.R')
-source('./src/lib/rgb2col.R')
-source('./src/lib/SavePlot.R')
+# source('./src/lib/GetColorTable.R')
+# source('./src/lib/TintShade.R')
+# source('./src/lib/SavePlot.R')
+# source('./src/lib/SortGroups.R')
+# source('./src/lib/rgb2col.R')
+
 source('./src/lib/AddOrgData.R')
 source('./src/lib/LoadAcsesData.R')
 source('./src/lib/RelabelAgebands.R')
@@ -36,10 +37,3 @@ source('./src/lib/RelabelPaybands.R')
 source('./src/lib/RelabelGrades.R')
 
 yearlabels <- c('2008','2009','2010\nSR10 baseline','2011','2012','2013')
-
-IfGBasecols <- c('#37424a','#00ccff','#d40072','#83389b','#7a9393','#457e81','#be8b5e')
-IfGcols <- TintShade(IfGBasecols,tints=c(.5,.25),hexin=T,)
-dimnames(IfGcols) <- NULL
-rm(IfGBasecols)
-
-source('./src/lib/custom_themes.R')
