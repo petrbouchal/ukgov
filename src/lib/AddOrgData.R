e57d7a43367a7adf3d1bb1ac1248a9ee1ee406c0
Petr Bouchal <pbouchal@gmail.com>
2014-09-11 19:41:22 +0100

    Switch ACSES system to 'managed' bounds

--- src/lib/AddOrgData.R
+++ src/lib/AddOrgData.R
@@ -1,6 +1,6 @@
 # Fn: Load org-group data -------------------------------------------------
 
-AddOrgData <- function (dataset, whitehallonly=FALSE) {
+AddOrgData <- function (dataset, whitehallonly=FALSE, managedonly = FALSE) {
   require(data.table)
   require(dplyr)
   # [Blah function description]
@@ -8,8 +8,9 @@ AddOrgData <- function (dataset, whitehallonly=FALSE) {
   # Args:
   #   dataset: Description of Arg1
   #   whitehallonly: Description of Arg2
-  orgs <- fread('./data-input/acses_orgs_fordatafrom2008to2013.csv')
+  orgs <- fread('./data-input/acses_orgs_fordatafrom2008to2013_managedbounds.csv')
   dataset <- merge(dataset,orgs, by='Organisation',all.x=TRUE)
+  dataset <- dataset[dataset$Include=='Yes',]
   if(whitehallonly) {
     dataset$Whitehall[dataset$Group=='HMRC'] <- 'WH'
     dataset$Whitehall[dataset$Group=='DWP'] <- 'WH'
@@ -17,6 +18,8 @@ AddOrgData <- function (dataset, whitehallonly=FALSE) {
     dataset$Whitehall[dataset$Group=='MoD'] <- 'WH'
     dataset <- dataset[dataset$Whitehall=='WH' | dataset$Whitehall=='Total',]
   }
-  dataset <- dataset[dataset$Include=='Yes',]
+  if(managedonly) {
+    dataset <- dataset[dataset$Managed==TRUE,]
+  }
   return(dataset)
 }
--- src/lib/LoadAcsesData.R
+++ src/lib/LoadAcsesData.R
@@ -16,6 +16,8 @@ LoadAcsesData <- function (file_name, location='home') {
   dataset$new1 <- NULL
   dataset$count <- as.numeric(as.character(dataset$value))
   dataset <- unique(dataset) # removes duplicate lines for DfE, DfID, Ofsted and GEO
+  # remove duplicate line in DfE
+  dataset <- dataset[!(dataset$Organisation == 'Education, Department for' & dataset$Date == 2013),]
   dataset$value <- NULL
   dataset$flag <- NULL
   dataset$value.type <- NULL
