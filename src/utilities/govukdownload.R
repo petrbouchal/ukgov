3c339711e0d778a4c5bf2647267d0c18ddd9eb0e
Petr Bouchal <pbouchal@gmail.com>
2014-09-22 17:26:54 +0100

    start function for grabbing files from data.gov.uk

--- src/utilities/govukdownload.R
+++ src/utilities/govukdownload.R
@@ -1,8 +1,14 @@
 url <- 'http://data.gov.uk/dataset/staff-organograms-and-pay-department-for-education'
 apiurl <- 'http://data.gov.uk/api/2/rest/package/staff-organograms-and-pay-department-for-education'
 
-library(jsonlite)
 data <- fromJSON(apiurl)
 
-data$resources$url
 
+graballfromdatagovuk <- function(url, extension, pattern) {
+  require(jsonlite)
+  
+  # read in data
+  data <- fromJSON(jsonurl)
+  data$resources$url
+  
+}
