8b92225c9c3af17126477882ad5d8a0e1be5d5ad
Petr Bouchal <pbouchal@gmail.com>
2014-10-14 19:00:54 +0100

    Fix bug in RelabelAgebands()

--- src/lib/RelabelAgebands.R
+++ src/lib/RelabelAgebands.R
@@ -3,8 +3,8 @@
 
 RelabelAgebands <- function (dataset) {
   dataset$Age.band <- gsub('Aged ','',dataset$Age.band)
-  dataset$Age.band <- gsub(' and over','+',dataset$Age.band)
-  dataset$Age.band <- gsub('60-64','65+',dataset$Age.band)
+  dataset$Age.band <- gsub('65 and over','60+',dataset$Age.band)
+  dataset$Age.band <- gsub('60-64','60+',dataset$Age.band)
   dataset$Age.band <- gsub('16-19','< 29',dataset$Age.band)
   dataset$Age.band <- gsub('20-29','< 29',dataset$Age.band)
   return(dataset)
