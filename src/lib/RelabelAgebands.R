f245f6d5604276222d4f0344fd56c42be58b2fab
Petr Bouchal <pbouchal@gmail.com>
2014-10-14 12:44:37 +0100

    Merge 60-64 and 65+ age bands

--- src/lib/RelabelAgebands.R
+++ src/lib/RelabelAgebands.R
@@ -3,7 +3,8 @@
 
 RelabelAgebands <- function (dataset) {
   dataset$Age.band <- gsub('Aged ','',dataset$Age.band)
-  dataset$Age.band <- gsub('and over','+',dataset$Age.band)
+  dataset$Age.band <- gsub(' and over','+',dataset$Age.band)
+  dataset$Age.band <- gsub('60-64','65+',dataset$Age.band)
   dataset$Age.band <- gsub('16-19','< 29',dataset$Age.band)
   dataset$Age.band <- gsub('20-29','< 29',dataset$Age.band)
   return(dataset)
