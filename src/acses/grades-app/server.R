6f40a0e25a68c78e0895533f1c4e96eab484f8fa
Petr Bouchal <pbouchal@gmail.com>
2014-10-24 01:19:57 +0100

    functioning grade shiny app x17 depts

--- src/acses/grades-app/server.R
+++ src/acses/grades-app/server.R
@@ -1,9 +1,8 @@
 library(ggvis)
 library(dplyr)
 server = shinyServer(function(input, output) {
-  load('./gradedata.rda')
-  gd2 <- gradedata[gradedata$left==FALSE,]
-  gd2 <- arrange(gd2, Date, Civil.Service.grad)
+  load('./gradedata2.rda')
+  gd2 <- arrange(gradedata2, Date, Civil.Service.grad)
   visdata1y <- reactive({
     #      gd2 <- gd2[gd2$Group==input$select_dept,]
     gd3 <- gd2[gd2$Date==input$slider_date,]
@@ -12,67 +11,229 @@ server = shinyServer(function(input, output) {
     } else {gd3}
   })
   scalemin <- min(gd2$share2, na.rm=T)
-  data1 <- reactive(visdata1y()[visdata1y()$Group=='HMT',])
-  data2 <- reactive(visdata1y()[visdata1y()$Group=='DCMS',])
-  data2 <- reactive(visdata1y()[visdata1y()$Group=='BIS',])
-  data3 <- reactive(visdata1y()[visdata1y()$Group=='DWP',])
-  data4 <- reactive(visdata1y()[visdata1y()$Group=='DECC',])
-  data5 <- reactive(visdata1y()[visdata1y()$Group=='DfID',])
-  data7 <- reactive(visdata1y()[visdata1y()$Group=='DCLG',])
-  data6 <- reactive(visdata1y()[visdata1y()$Group=='CO',])
-  data1 %>%
-    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
+  
+  data_hmt <- reactive(visdata1y()[visdata1y()$Group=='HMT',])
+  data_dcms <- reactive(visdata1y()[visdata1y()$Group=='DCMS',])
+  data_bis <- reactive(visdata1y()[visdata1y()$Group=='BIS',])
+  data_dwp <- reactive(visdata1y()[visdata1y()$Group=='DWP',])
+  data_decc <- reactive(visdata1y()[visdata1y()$Group=='DECC',])
+  data_dfid <- reactive(visdata1y()[visdata1y()$Group=='DfID',])
+  data_dclg <- reactive(visdata1y()[visdata1y()$Group=='DCLG',])
+  data_co <- reactive(visdata1y()[visdata1y()$Group=='CO',])
+  data_mod <- reactive(visdata1y()[visdata1y()$Group=='MoD',])
+  data_moj <- reactive(visdata1y()[visdata1y()$Group=='MoJ',])
+  data_defra <- reactive(visdata1y()[visdata1y()$Group=='Defra',])
+  data_dft <- reactive(visdata1y()[visdata1y()$Group=='DfT',])
+  data_dfe <- reactive(visdata1y()[visdata1y()$Group=='DfE',])
+  data_hmrc <- reactive(visdata1y()[visdata1y()$Group=='HMRC',])
+  data_fco <- reactive(visdata1y()[visdata1y()$Group=='FCO',])
+  data_dh <- reactive(visdata1y()[visdata1y()$Group=='DH',])
+  data_ho <- reactive(visdata1y()[visdata1y()$Group=='HO',])
+  
+  data_decc %>%
+    ggvis(x=~Civil.Service.grad) %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%
     scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis1')
-  data2 %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DECC', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_decc')
+  
+  data_dcms %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%
     scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis2')        
-  data3 %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DCMS', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dcms')        
+  
+  data_bis %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
-    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis3')
-  data4 %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='BIS', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_bis')
+  
+  data_dwp %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
-    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis4')
-  data5 %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DWP', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dwp')
+  
+  data_hmt %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
-    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis5')          
-  data6 %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='HMT', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_hmt')          
+  
+  data_dfid %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
-    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis6')                 
-  data7 %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DfID', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dfid')                 
+
+  data_fco %>%
     ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
-    layer_ribbons(y=~share/2, y2=~share2) %>%
-    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
-    add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%
-    add_axis('y',tick_size_major = 0, grid=FALSE, title='') %>%
-    set_options(width=200, height = 200, padding=padding(20,20,20,20)) %>%
-    bind_shiny('ggvis7')
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='FCO', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_fco')
+
+  data_dft %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DfT', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dft')
+
+  data_dfe %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DfE', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dfe')
+
+  data_ho %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='HO', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_ho')
+
+  data_hmrc %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='HMRC', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_hmrc')
+
+  data_defra %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='Defra', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_defra')
+
+  data_moj %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='MoJ', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_moj')
+
+  data_mod %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='MoD', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_mod')
+
+  data_co %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='CO', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_co')
+
+  data_dh %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DH', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dh')
+
+  data_dclg %>%
+    ggvis(x=~Civil.Service.grad, fill:="#00ccff") %>%
+    layer_ribbons(y=~share_2010/2, y2=~share2_2010, fill:='gray', opacity:=.3) %>%
+    layer_ribbons(y=~share/2, y2=~share2, fill:="#00ccff", opacity:=.4) %>%    scale_numeric('y', domain=c(scalemin, scalemin*-1)) %>%
+    add_axis('x',tick_size_major = 0, grid=FALSE,
+             title='DCLG', title_offset = -100, layer = 'front',
+             properties=axis_props(axis = list(stroke = "red", strokeWidth = 0),
+                                   title=list(fontSize=17, fill='white'))) %>%
+    hide_axis('y') %>%
+    set_options(width=200, height = 200, padding=padding(0,0,20,10), resizable=FALSE) %>%
+    bind_shiny('ggvis_dclg')
+
 })
--- src/acses/grades-app/ui.R
+++ src/acses/grades-app/ui.R
@@ -1,29 +1,50 @@
 library(ggvis)
 library(dplyr)
 
-ui = fixedPage(responsive = TRUE,
+ui = fluidPage(responsive = TRUE,
+               title='The changing shape of government departments',
+               titlePanel(title='The changing shape of the Civil Service',
+                          windowTitle='The changing shape of the Civil Service'),
+               HTML('<p>This is an interactive addendum to our 
+                    <a href=\'\'>blog</a> on the structure of the 
+                    Civil Service.'),
   fixedRow(
-      column(width=3,
-        sliderInput(min=2010, max=2014, step=1,animate = TRUE, value=2010,
-                    inputId = 'slider_date', label='Year',format='####',
-                    width='150px')
-                        )
+      column(width=12,
+        div(style='width:180px;',
+        sliderInput(min=2010, max=2014, step=1,
+                    animate = animationOptions(interval=1600,
+                                               playButton = HTML('<div style=\'margin-top:-25px; text-align: center; margin-left:150px;  font-size:22pt;\'>&ogt;</div>'),
+                                               pauseButton = HTML('<div style=\'margin-top:-25px; text-align: center; margin-left:150px; font-size:22pt;\'>&opar; </div>')),
+                    value=2010,
+                    inputId = 'slider_date', label=NULL,format='####',
+                    width='150px', ticks=NULL)
+                        ))),
+  fixedRow(
+      column(width=3,ggvisOutput('ggvis_decc')),
+      column(width=3,ggvisOutput('ggvis_dfid')),
+      column(width=3,ggvisOutput('ggvis_hmt')),
+      column(width=3,ggvisOutput('ggvis_dh'))
+    ),
+  fixedRow(
+      column(width=3,ggvisOutput('ggvis_co')),
+      column(width=3,ggvisOutput('ggvis_fco')),
+      column(width=3,ggvisOutput('ggvis_defra')),
+      column(width=3,ggvisOutput('ggvis_dfe'))
 
     ),
   fixedRow(
-      column(width=4,ggvisOutput('ggvis1')),
-      column(width=4,ggvisOutput('ggvis2')),
-      column(width=4,ggvisOutput('ggvis3'))
-
+      column(width=3,ggvisOutput('ggvis_dcms')),
+      column(width=3,ggvisOutput('ggvis_dft')),
+      column(width=3,ggvisOutput('ggvis_dclg')),
+      column(width=3,ggvisOutput('ggvis_bis'))
     ),
   fixedRow(
-      column(width=4,ggvisOutput('ggvis4')),
-      column(width=4,ggvisOutput('ggvis5')),
-      column(width=4,ggvisOutput('ggvis6'))
-
+      column(width=3,ggvisOutput('ggvis_mod')),
+      column(width=3,ggvisOutput('ggvis_hmrc')),
+      column(width=3,ggvisOutput('ggvis_dwp')),
+      column(width=3,ggvisOutput('ggvis_ho'))
     ),
   fixedRow(
-      column(width=4,ggvisOutput('ggvis7'))
-
+      column(width=3,ggvisOutput('ggvis_moj'))
     )
 )
