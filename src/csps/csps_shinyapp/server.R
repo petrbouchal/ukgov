901e7e64f86effde43998e06652318460f179b93
Petr Bouchal <pbouchal@gmail.com>
2014-11-05 18:04:27 +0000

    improvement to CSPS shiny app

--- src/csps/csps_shinyapp/server.R
+++ src/csps/csps_shinyapp/server.R
@@ -4,24 +4,26 @@ library(ggvis)
 
 shinyServer(function(input, output) {
   csps <- read.csv('csps.csv')
+  questionkey <- read.csv('questionkey.csv')
+  csps <- merge(csps, questionkey, all.x=TRUE)
   csps$value <- csps$value*100
   csps$split <- relevel(csps$split, 'SCS')
   
   output$dimensionsList <- renderUI({
     dimensions <- unique(as.character(csps$dimension))
     selectInput("dimension2", "Choose dimension", as.list(dimensions),
-                selected='Age')
+                selected='Grade')
   })
   
   output$measuresList <- renderUI({
-    measures <- unique(as.character(csps$measure))
+    measures <- unique(as.character(csps$questiontext))
     selectInput("measure2", "Choose measure", as.list(measures),multiple = TRUE,
-                selected='Employee engagement index')
+                selected='I feel that my pay adequately reflects my performance')
   })
   
-  cspsshort <- csps[csps$measure=='Employee engagement index' &
+  cspsshort <- csps[csps$questiontext=='Employee engagement index' &
                       csps$dimension=='Age',]
-  cspsshort$measure <- droplevels(cspsshort$measure)
+  cspsshort$questiontext <- droplevels(cspsshort$questiontext)
   cspsshort$dimension <- droplevels(cspsshort$dimension)
   cspsshort$split <- droplevels(cspsshort$split)
   
@@ -31,10 +33,10 @@ shinyServer(function(input, output) {
 #     }
     meas <- input$measure2
     dimen <- input$dimension2
-    csps2 <- csps[csps$dimension==dimen & csps$measure %in% meas,]
+    csps2 <- csps[csps$dimension==dimen & csps$questiontext %in% meas,]
     csps2$dimension <- droplevels(csps2$dimension) 
     csps2$split <- droplevels(csps2$split)
-    csps2$measure <- droplevels(csps2$measure)
+    csps2$questiontext <- droplevels(csps2$questiontext)
     if(nrow(csps2) == 0){
       return(cspsshort)
     }
@@ -42,7 +44,7 @@ shinyServer(function(input, output) {
     }) 
   
   tooltip <- function(data) {
-    paste0('<i>',data$split,'</i>', '<br />', data$measure, ': ', '<b>',data$value,'</b>')
+    paste0('<i>',data$split,'</i>', '<br />', data$questiontext, ': ', '<b>',data$value,'</b>')
   }
 
   colourscale <- c("#00ccff", "#37424a", "#d40072", "#83389b", 
@@ -51,10 +53,10 @@ shinyServer(function(input, output) {
                    "#dde4e4", "#d0dedf", "#eee2d6") 
 
   csps3 %>%
-    ggvis(~split, ~value, stroke = ~measure, fillOpacity.hover := 1,
+    ggvis(~split, ~value, stroke = ~questiontext, fillOpacity.hover := 1,
           fillOpacity := .7, strokeOpacity := .7, strokeOpacity.hover :=1) %>%
     layer_lines(strokeWidth:=3) %>%
-    layer_points(fill = ~measure, size := 50, size.hover := 90) %>%
+    layer_points(fill = ~questiontext, size := 50, size.hover := 90) %>%
 #     layer_bar(width=.9) %>%
     add_axis('x',tick_size_major = 0, grid=FALSE, title='') %>%  
     add_axis('y',tick_size_major = 0, grid=TRUE, values=c(0,25,50,75,100),
@@ -66,6 +68,7 @@ shinyServer(function(input, output) {
     add_tooltip(tooltip) %>%
     scale_nominal(property = 'x') %>%
     scale_numeric('y') %>%
+    set_options(padding = padding(20, 150, 25, 30)) %>%
     add_legend(c('stroke','fill')) %>%
     bind_shiny('ggvis')
   
