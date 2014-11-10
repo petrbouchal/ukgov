b41e15a568bf88d8c0d49a759f0b2519efa9e609
Petr Bouchal <pbouchal@gmail.com>
2014-11-10 17:18:35 +0000

    move res man code

--- src/resource-management-models/ClusteringDepts.R
+++
@@ -1,8 +0,0 @@
-pols5 <- read.csv('./data-input/Outputs_roughscores_6cols.csv')
-
-row.names(pols5) <- pols5$Dept
-
-hc5=hclust(dist(pols5))
-
-par(mar=c(2,1,1,3))
-plot(as.dendrogram(hc5),horiz=T,type='rectangle')
