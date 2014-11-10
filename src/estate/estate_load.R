a3b3ca67e21629cb1395384ef26d3e063d0e222a
Petr Bouchal <pbouchal@gmail.com>
2014-11-10 12:33:20 +0000

    Moving estate code to https://github.com/petrbouchal/ukgov-estate

--- src/estate/estate_load.R
+++
@@ -1,9 +0,0 @@
-library(xlsx)
-
-estpath <- 'P:/Research & Learning/Research/19. Transforming Whitehall/Whitehall Monitor/Data Sources/Estate/'
-estfilename <- 'Estate v0.2.xlsx'
-estfilepath <- paste0(estpath,estfilename)
-est <- read.xlsx(estfilepath,sheetName = 'BenchmarkAll',startRow = 1)
-names(est)
-
-
